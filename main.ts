62ed9666cd97ae77c900b693b241b7b5e1b52e1c
filipvyroubal <filip.vyroubal.025@pslib.cz>
2026-05-04 13:18:50 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -19,20 +19,73 @@ function NakresliVzor(vzor : Array<Array<boolean>>):void{
         }
     }
 }
-function PassiveToStarted(min:number,max:number):number{
-    NakresliVzor(hodiny);
-    music.play(music.tonePlayable(Note.C, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-    basic.pause(randint(min,max));
-    return stavy.Started;
+function Win(player : string){
+    basic.showString(player)
+    control.inBackground(() => {
+        music.play(music.stringPlayable("F# G# A# R", 360), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("F# G# A# R", 360), music.PlaybackMode.UntilDone)
+    })
 }
-function WasPressedBefore():void{
-    if(input.buttonIsPressed(Button.A) && input.buttonIsPressed(Button.B)){
-        basic.showIcon(IconNames.Sad)
+function Tie(ic : IconNames){
+    basic.showIcon(ic)
+    control.inBackground(() => {
+        music.play(music.stringPlayable("E E R G G R D D R A A R", 360), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("E E R G G R D D R A A R", 360), music.PlaybackMode.UntilDone)
+    })
+}
+function WasPressedBefore(): boolean {
+    if (input.buttonIsPressed(Button.A) && input.buttonIsPressed(Button.B)) {
+        Tie(IconNames.Sad)
+        return true;
+    } else if (input.buttonIsPressed(Button.A) && !input.buttonIsPressed(Button.B)) {
+        Win("B")
+        return true
+    } else if (!input.buttonIsPressed(Button.A) && input.buttonIsPressed(Button.B)) {
+        Win("A")
+        return true
+    }
+    else{
+        return false
     }
 }
+function PassiveToStarted(min:number,max:number):number{
+    NakresliVzor(hodiny);
+    control.inBackground(() => music.play(music.tonePlayable(Note.C, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone))
+    basic.pause(randint(min,max)*1000);
+    AktualniStav = stavy.Started;
+    return WasPressedBefore() ? stavy.Passive : stavy.Started;
+}
+function StartedToRunning(): number{
+    basic.showIcon(IconNames.Pitchfork);
+    control.inBackground(() => music.playTone(Note.FSharp5,200));
+    return stavy.Running;
+}
+
 input.onButtonPressed(Button.A, function() {
     switch(AktualniStav){
         case stavy.Passive:
+            basic.clearScreen();
             AktualniStav = PassiveToStarted(3,6);
+            basic.clearScreen();
+            AktualniStav = StartedToRunning();  
+    }
+})
+basic.forever(function() {
+    switch(AktualniStav){
+        case stavy.Running:
+            let pressedA = input.buttonIsPressed(Button.A)
+            let pressedB = input.buttonIsPressed(Button.B)
+            if(pressedA && pressedB){
+                Tie(IconNames.Square)
+                AktualniStav = stavy.Passive
+            }else if(pressedA){
+                Win("A")
+                AktualniStav = stavy.Passive
+            } else if (pressedB) {
+                Win("B")
+                AktualniStav = stavy.Passive
+            }else{
+                basic.pause(100)
+            }
     }
 })
